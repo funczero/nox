e5a30ba5dd208ea44531638935fe72b168bebe38
FuncZero <contato@funczero.xyz>
2025-04-11 21:07:06 +0000

    Atualização automática

---
+++ register.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var registerCommands_js_1 = require("./src/handlers/registerCommands.js");
+(0, registerCommands_js_1.registerCommands)();
